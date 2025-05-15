4d3c1afb4ca6c7236f75195381b1ff8e7bf4e82c
greenlaser <55558448+greeenlaser@users.noreply.github.com>
2025-05-15 16:10:16 +0300

    improved build system

--- testproj_linux.sh
+++ testproj_linux.sh
@@ -3,47 +3,6 @@
 # Set root folder
 ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-# Set shell script paths
-RELEASE_SH="${ROOT_DIR}/build_linux_release.sh"
-DEBUG_SH="${ROOT_DIR}/build_linux_debug.sh"
-COPY_SH="${ROOT_DIR}/_external_shared/copy_linux.sh"
-
-# Confirm if shell script paths are valid
-if [[ ! -f "$COPY_SH" ]]; then
-    echo "[ERROR] Copy shell script does not exist: $COPY_SH"
-    read -r -p "Press enter to exit..."
-    exit 1
-fi
-if [[ ! -f "$RELEASE_SH" ]]; then
-    echo "[ERROR] Release shell script does not exist: $RELEASE_SH"
-    read -r -p "Press enter to exit..."
-    exit 1
-fi
-if [[ ! -f "$DEBUG_SH" ]]; then
-    echo "[ERROR] Debug shell script does not exist: $DEBUG_SH"
-    read -r -p "Press enter to exit..."
-    exit 1
-fi
-
-# Copy external shared
-if ! bash "${COPY_SH}"; then
-    echo "[FATAL] Copy failed!"
-    read -r -p "Press enter to exit..."
-    exit 1
-fi
-
-# Compile in release and debug mode
-if ! bash "${RELEASE_SH}"; then
-    echo "[FATAL] Release build failed!"
-    read -r -p "Press enter to exit..."
-    exit 1
-fi
-if ! bash "${DEBUG_SH}"; then
-    echo "[FATAL] Debug build failed!"
-    read -r -p "Press enter to exit..."
-    exit 1
-fi
-
 # Set headers folder
 HEADER_DIR="${ROOT_DIR}/build-release/include"
 
--- testproj_windows.bat
+++ testproj_windows.bat
@@ -6,55 +6,6 @@ cd "%PROJECT_ROOT%"
 set "BUILD_RELEASE=%PROJECT_ROOT%build-release"
 set "BUILD_DEBUG=%PROJECT_ROOT%build-debug"
 
-:: Remove old build folders
-if exist "%BUILD_RELEASE%" rmdir /S /Q "%BUILD_RELEASE%"
-if exist "%BUILD_DEBUG%" rmdir /S /Q "%BUILD_DEBUG%"
-
-echo =====================================
-echo [INFO] Copying external shared files...
-echo =====================================
-echo.
-
-cd _external_shared
-cmd /c "copy_windows.bat"
-if errorlevel 1 (
-    echo [ERROR] Copy failed.
-    pause
-    exit /b 1
-)
-cd ..
-
-echo =====================================
-echo [INFO] Building KalaWindow in Release mode...
-echo =====================================
-echo.
-
-cmd /c "build_windows_release.bat"
-if errorlevel 1 (
-    echo [ERROR] Release build failed.
-    pause
-    exit /b 1
-)
-
-echo.
-echo =====================================
-echo [INFO] Building KalaWindow in Debug mode...
-echo =====================================
-echo.
-
-cmd /c "build_windows_debug.bat"
-if errorlevel 1 (
-    echo [ERROR] Debug build failed.
-    pause
-    exit /b 1
-)
-
-echo.
-echo =====================================
-echo [SUCCESS] Finished building and installing KalaWindow!
-echo =====================================
-echo.
-
 set "ORIGIN_RELEASE_DLL=%BUILD_RELEASE%\bin\KalaWindow.dll"
 set "ORIGIN_RELEASE_LIB=%BUILD_RELEASE%\lib\KalaWindow.lib"
 set "ORIGIN_DEBUG_DLL=%BUILD_DEBUG%\bin\KalaWindowD.dll"
