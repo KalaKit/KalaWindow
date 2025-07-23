59e9d810865e5ac3189445052f67175e9b2f56a7
greenlaser <55558448+greeenlaser@users.noreply.github.com>
2025-07-24 00:14:32 +0300

    getter-setters for vulkan shader structs

--- include/graphics/vulkan/shader_vulkan.hpp
+++ include/graphics/vulkan/shader_vulkan.hpp
@@ -361,6 +361,42 @@ namespace KalaWindow::Graphics::Vulkan
 
 		Window* GetTargetWindow() { return targetWindow; }
 
+		VulkanData_VertexInputState& GetVulkanVertexInputStruct() { return vulkan_vi_struct; }
+		void SetVulkanVertexInputStruct(VulkanData_VertexInputState vulkan_new_vi_struct)
+		{
+			vulkan_vi_struct = vulkan_new_vi_struct;
+		}
+
+		VulkanData_InputAssemblyState& GetVulkanInputAssemblyStruct() { return vulkan_ia_struct; }
+		void SetVulkanInputAssemblyStruct(VulkanData_InputAssemblyState vulkan_new_ia_struct)
+		{
+			vulkan_ia_struct = vulkan_new_ia_struct;
+		}
+
+		VulkanData_RasterizationState& GetVulkanRasterizationStruct() { return vulkan_r_struct; }
+		void SetVulkanRasterizationStruct(VulkanData_RasterizationState vulkan_new_r_struct)
+		{
+			vulkan_r_struct = vulkan_new_r_struct;
+		}
+
+		VulkanData_ColorBlendState& GetVulkanColorBlendStruct() { return vulkan_cb_struct; }
+		void SetVulkanColorBlendStruct(VulkanData_ColorBlendState vulkan_new_cb_struct)
+		{
+			vulkan_cb_struct = vulkan_new_cb_struct;
+		}
+
+		VulkanData_DepthStencilState& GetVulkanDepthStencilStruct() { return vulkan_ds_struct; }
+		void SetVulkanDepthStencilStruct(VulkanData_DepthStencilState vulkan_new_ds_struct)
+		{
+			vulkan_ds_struct = vulkan_new_ds_struct;
+		}
+
+		VulkanData_TesselationState& GetVulkanTesselationStruct() { return vulkan_t_struct; }
+		void SetVulkanTesselationStruct(VulkanData_TesselationState vulkan_new_t_struct)
+		{
+			vulkan_t_struct = vulkan_new_t_struct;
+		}
+
 		unsigned int GetPipeline() const { return pipeline; }
 		unsigned int GetLayout() const { return layout; }
 		unsigned int GetDescriptorSetLayout() const { return descriptorSetLayout; }
@@ -496,6 +532,13 @@ namespace KalaWindow::Graphics::Vulkan
 		string name{};
 		function<void()> drawCommands{}; //The commands relative to this shader that are called inside bind
 		Window* targetWindow{};          //The window this shader is attached to
+
+		VulkanData_VertexInputState vulkan_vi_struct{};
+		VulkanData_InputAssemblyState vulkan_ia_struct{};
+		VulkanData_RasterizationState vulkan_r_struct{};
+		VulkanData_ColorBlendState vulkan_cb_struct{};
+		VulkanData_DepthStencilState vulkan_ds_struct{};
+		VulkanData_TesselationState vulkan_t_struct{};
 
 		uintptr_t pipeline{};            //vkpipeline
 		uintptr_t layout{};              //vkpipelinelayout
