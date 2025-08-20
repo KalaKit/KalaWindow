47d731ad16f2b5c5e0ea5618822c6b0385f5121c
greenlaser <55558448+greeenlaser@users.noreply.github.com>
2025-08-20 23:48:07 +0300

    some minor tweaks

---
+++ include/core/glm_global.hpp
@@ -0,0 +1,15 @@
+//Copyright(C) 2025 Lost Empire Entertainment
+//This program comes with ABSOLUTELY NO WARRANTY.
+//This is free software, and you are welcome to redistribute it under certain conditions.
+//Read LICENSE.md for more information.
+
+#pragma once
+
+#include "glm/glm.hpp"
+
+using glm::vec2;
+using glm::vec3;
+using glm::vec4;
+using glm::mat2;
+using glm::mat3;
+using glm::mat4;
