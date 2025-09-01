f791062e9501c12640ceffbf7392e292a387d9b2
greenlaser <55558448+greeenlaser@users.noreply.github.com>
2025-09-01 10:25:32 +0300

    Update imgui_export.hpp

--- _external_shared/imgui/imgui_export.hpp
+++ _external_shared/imgui/imgui_export.hpp
@@ -1,8 +1,3 @@
-//Copyright(C) 2025 Lost Empire Entertainment
-//This program comes with ABSOLUTELY NO WARRANTY.
-//This is free software, and you are welcome to redistribute it under certain conditions.
-//Read LICENSE.md for more information.
-
 #pragma once
 
 #ifdef _WIN32
